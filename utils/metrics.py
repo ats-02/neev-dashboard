e74b7f5f41b38acfbcc059bc7f208a90fb02c003
Athul Suresh <144668907+ats-02@users.noreply.github.com>
2026-06-04 17:26:42 +0530

    Update metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,1 +1,19 @@
+def production_summary(df):
+
+    return {
+
+        "total_shots": len(df),
+
+        "avg_cycle_time":
+        round(
+            df["cycle_time"].mean(),
+            2
+        ),
+
+        "avg_cast_pressure":
+        round(
+            df["cast_pressure"].mean(),
+            2
+        )
+    }
 
