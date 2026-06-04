d1ea02fc3e2d71f7c5308cbc1eedc76ad39e1f04
Athul Suresh <144668907+ats-02@users.noreply.github.com>
2026-06-04 17:24:10 +0530

    Add GOLDEN_ZONE configuration settings

--- config/settings.py
+++ config/settings.py
@@ -1,1 +1,18 @@
+GOLDEN_ZONE = {
+
+    "lo_v": (0.25, 0.27),
+
+    "hi_v": (3.90, 4.20),
+
+    "cast_pressure": (930, 950),
+
+    "intensify": (230, 240),
+
+    "p_rise": (40, 60),
+
+    "biscuit_thick": (18, 24),
+
+    "cycle_time": (45, 50)
+
+}
 
