ea0a7a9da01e091ecfcb114d32df1a2e0f227162
Athul Suresh <144668907+ats-02@users.noreply.github.com>
2026-06-04 17:26:13 +0530

    Add plunger_health function to calculate health

--- utils/maintenance.py
+++ utils/maintenance.py
@@ -1,1 +1,20 @@
+def plunger_health(current_shots):
+
+    expected_life = 100000
+
+    remaining = expected_life - current_shots
+
+    health = (
+        remaining / expected_life
+    ) * 100
+
+    return {
+
+        "remaining": remaining,
+
+        "health": round(
+            health,
+            2
+        )
+    }
 
